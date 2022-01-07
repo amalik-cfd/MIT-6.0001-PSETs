de9420b58fdfeda8a78c1b8f9042f13e667c4d3d
Abdul Malik Huzaifa <huzaifa.mk94@gmail.com>
2022-01-07 07:55:45 -0500

    Problem set 0
    Basic Assignment

---
+++ PSET0.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import numpy as np
+import math
+print('Hello, please enter a number x.')
+x = int(input())
+print('Now please enter another number y.')
+y = int(input())
+z = x**y
+w = math.log(x, 2)
+print('x**y = ', z)
+print('log(x) = ', w)
